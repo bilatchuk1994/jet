d2837d8bbf1324c885aa80d2c036245cca8152cf
bilatchuk1994 <sefan1994@gmail.com>
2020-06-01 12:32:24 +0300

    Баннера ЛК

---
+++ banners4.php
@@ -0,0 +1,258 @@
+<?php include("header.php"); ?>
+
+<main class="main">
+
+<!-- Новий баннер -->
+
+
+<section class="banner banner-lots lots-for-payment">
+  <div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+          <div class="breadcrums">
+          <ul>
+            <li><a href="">Главная</a></li>
+            <li><span>></span></li>
+            <li><a href="">каталог</a></li>
+            <li><span>></span></li>
+            <li>лоты на оплату</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    <div class="row d-flex align-items-center">
+      <div class="col-8 col-lg-9 col-md-9">
+        <h3>Лоты на оплату</h3>
+        <p>Здесь находятся все выигранные лоты, которые требуют оплаты. Нажмите на кнопку "оплатить", чтобы завершить процесс покупки лота</p>
+      </div>
+      <div class="col-4 col-lg-3 col-md-3">
+        <div class="banner-img">
+          <img src="banners-img/ruble.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="banner banner-tv">
+  <div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+          <div class="breadcrums">
+          <ul>
+            <li><a href="">Главная</a></li>
+            <li><span>></span></li>
+            <li><a href="">каталог</a></li>
+            <li><span>></span></li>
+            <li>Гарнитуры для PS4 Pro / PS4. Xbox One & More. 951 Единицы</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    <div class="row">
+      <div class="col-8 col-lg-7 col-md-8">
+        <h3>Ликвидации телевизоров <br> от крупнейших ритейлеров <br><br></h3>
+        <p>Покупайте выгодно и зарабатывайте от 100% прибыли на их перепродажи</p>
+      </div>
+      <div class="col-4 col-lg-5 col-md-4">
+        <div class="banner-img">
+          <img src="img/banner-img39.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="banner banner-lots lots-truck">
+  <div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+          <div class="breadcrums">
+          <ul>
+            <li><a href="">Главная</a></li>
+            <li><span>></span></li>
+            <li><a href="">каталог</a></li>
+            <li><span>></span></li>
+            <li>лоты на оплату</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    <div class="row d-flex align-items-center">
+      <div class="col-8 col-lg-9 col-md-9">
+        <h3>Лоты в процессе доставки</h3>
+        <p>Здесь находятся все лоты, которые находяться в процессе доставки вам</p>
+      </div>
+      <div class="col-4 col-lg-3 col-md-3">
+        <div class="banner-img">
+          <img src="banners-img/truck.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="banner banner-lots lots-right">
+  <div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+          <div class="breadcrums">
+          <ul>
+            <li><a href="">Главная</a></li>
+            <li><span>></span></li>
+            <li><a href="">каталог</a></li>
+            <li><span>></span></li>
+            <li>лоты на оплату</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    <div class="row d-flex align-items-center">
+      <div class="col-8 col-lg-9 col-md-9">
+        <h3>Доставленные лоты</h3>
+        <p>Здесь находятся все лоты, которые были успешно доставлены вам</p>
+      </div>
+      <div class="col-4 col-lg-3 col-md-3">
+        <div class="banner-img">
+          <img src="banners-img/right.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="banner banner-lots lots-favorites">
+  <div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+          <div class="breadcrums">
+          <ul>
+            <li><a href="">Главная</a></li>
+            <li><span>></span></li>
+            <li><a href="">каталог</a></li>
+            <li><span>></span></li>
+            <li>лоты на оплату</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    <div class="row d-flex align-items-center">
+      <div class="col-8 col-lg-9 col-md-9">
+        <h3>Избранные лоты</h3>
+        <p>Здесь находятся лоты, которые вы пометили как избранные</p>
+      </div>
+      <div class="col-4 col-lg-3 col-md-3">
+        <div class="banner-img">
+          <img src="banners-img/favorites.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="banner banner-lots lots-legal-paper">
+  <div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+          <div class="breadcrums">
+          <ul>
+            <li><a href="">Главная</a></li>
+            <li><span>></span></li>
+            <li><a href="">каталог</a></li>
+            <li><span>></span></li>
+            <li>лоты на оплату</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    <div class="row d-flex align-items-center">
+      <div class="col-8 col-lg-9 col-md-9">
+        <h3>История ставок</h3>
+        <p>Здесь находятся все лоты, к которым вы делали ставки</p>
+      </div>
+      <div class="col-4 col-lg-3 col-md-3">
+        <div class="banner-img">
+          <img src="banners-img/legal-paper.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="banner banner-lots lots-clockwise">
+  <div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+          <div class="breadcrums">
+          <ul>
+            <li><a href="">Главная</a></li>
+            <li><span>></span></li>
+            <li><a href="">каталог</a></li>
+            <li><span>></span></li>
+            <li>лоты на оплату</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    <div class="row d-flex align-items-center">
+      <div class="col-8 col-lg-9 col-md-9">
+        <h3>История просмотров</h3>
+        <p>Здесь находятся последние просмотренные вами лоты</p>
+      </div>
+      <div class="col-4 col-lg-3 col-md-3">
+        <div class="banner-img">
+          <img src="banners-img/clockwise.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="banner banner-lots lots-law">
+  <div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+          <div class="breadcrums">
+          <ul>
+            <li><a href="">Главная</a></li>
+            <li><span>></span></li>
+            <li><a href="">каталог</a></li>
+            <li><span>></span></li>
+            <li>лоты на оплату</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    <div class="row d-flex align-items-center">
+      <div class="col-8 col-lg-9 col-md-9">
+        <h3>Активные ставки</h3>
+        <p>Здесь находятся все активные лоты, к которым вы сделали ставки</p>
+      </div>
+      <div class="col-4 col-lg-3 col-md-3">
+        <div class="banner-img">
+          <img src="banners-img/law.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="lots-none">
+<div class="container">
+    <div class="row">
+        <div class="col-12 <col-md-8></col-md-8> col-lg-6 offset-md-2 offset-lg-4">
+            В данный момент у вас нет ни одной активной ставки, перейдите в <a href="#">каталог</a> или воспользуйтесь поиском, чтобы подобрать интересующие вас лоты
+        </div>
+    </div>
+</div>    
+</section>
+</main>
+
+
+
+<?php include("footer.php"); ?>
+
+
+
+
