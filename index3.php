7600d58684b27a19c24fd8c45b9c04f6cac4e0de
bilatchuk1994 <sefan1994@gmail.com>
2020-07-23 16:39:16 +0300

    111

---
+++ index3.php
@@ -0,0 +1,203 @@
+<?php include("header.php"); ?>
+
+
+<!-- Новий баннер -->
+
+
+
+<section class="lots-list">
+<div class="container">
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="lot-title">
+        <h2>Оплачении лоти</h2>
+      </div>
+    </div>
+  </div>
+    <div class="row justify-content-center">
+        <div class="col-lg-9 12 col-md-8 col-lg-6">
+            <div class="product-block">
+              <div class="row product-row">
+                <div class="col-6 col-lg-5 col-md-5">
+                  <div class="product-img-flex">
+                  <a href class="product-big-img">
+                    <img class="lazyload" data-src="img/product-img.jpg" alt="">
+                  </a>
+                  <div>
+                    <a href="" class="small-img">
+                    <img class="lazyload" data-src="img/product-img.jpg" alt="">
+                  </a>
+                  <a href="" class="small-img">
+                    <img class="lazyload" data-src="img/product-img.jpg" alt="">
+                  </a>
+                  </div>
+                  </div>
+                  <div class="d-block d-md-none">
+                    <div class="medium-text">
+                    <p>До конца аукциона осталось:</p>
+                  </div>
+                  <h5>1д : 3ч : 30м</h5>
+                  <div class="cabinet-link">
+                        <a href="" class="make-bet">Готов к отгрузке</a>
+                      </div>
+                  </div>
+                </div>
+
+                <div class="col-6 col-lg-7 col-md-7">
+                  <div class="product-flex">
+                    <div>
+                      <div class="product-info-center">
+                  <a href=""><h3>Сплит-система Midea Blanc MSMA1D-24HRN1 /MOCA02-24HN1 on/off</h3></a>
+                  <ul class="product-list">
+                    <li><img src="img/paper.svg" alt=""><a href="">СМОТРЕTь СПЕЦИФИКАЦИЮ</a></li>
+                  </ul>
+                  <ul class="product-detail-list">
+                    <li><img src="img/product.svg" alt="">Количество товаров: <span>25</span></li>
+                  </ul>
+                  <div class="d-none d-md-block">
+                    <div class="medium-text">
+                    <p>До конца аукциона осталось:</p>
+                  </div>
+                  <h5>1д : 3ч : 30м</h5>
+                  </div>
+                  <div class="d-none d-md-block">
+                  <div class="cabinet-link">
+                        <a href="" class="make-bet">Готов к отгрузке</a>
+                      </div>
+                  </div>
+                </div>
+                  
+                    </div>
+                    <div>
+                      <div class="price-text">
+                    <p>Вы зарабатываете более: <br> <span class="price-t">2500 р.</span></p>
+                    <p>Розничная цена:  <br> <span class="price-m">2500 р.</span></p>
+                    <p class="last-p">Текущая ставка: <br> <span class="price-b">365 540 ₽</span> <span class="info-hover">Уже с НДС</span></p>
+                  </div>
+                    </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-lg-10">
+            <div class="popup-table-inner lot-table">
+    <div class="popup-scroll">
+    <table>
+      <tr>
+        <th>Товар</th>
+        <th>Кол-во</th>
+        <th>Розничная цена</th>
+        <th>Общая розничная цена</th>
+      </tr>
+      <tr>
+        <td><span><div><div class="lot-preview"><img src="img/table-icon.png" alt=""></div></div>Ноутбук ASUS X556UB-XO035T</span></td>
+        <td>1</td>
+        <td>34981 р</td>
+        <td>34981 р</td>
+      </tr>
+      <tr>
+        <th class="bottom-table"><div class="flex-end">Итого:</div></th>
+        <th class="bottom-table">5</th>
+        <th class="bottom-table"></th>
+        <th class="bottom-table">174 905 р.</th>
+      </tr>
+    </table>
+    </div>
+    <div class="button-group">
+      <div>
+        <a class="make-bet" href="">Распечатать спецификацию <img src="img/print.svg" alt=""></a>
+      </div>
+      <div>
+        <a class="make-bet" href="">скачать спецификацю <img src="img/download.svg" alt=""></a>
+      </div>
+      <div>
+        <a class="make-bet popup-btn" data-effect="mfp-zoom-in" href="#popup-table-edit">Редактировать спецификацию <img src="img/edit.svg" alt=""></a>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="popup-table-inner lot-table">
+    <div class="popup-scroll">
+    <table>
+      <tr>
+        <th>Товар</th>
+        <th>Кол-во</th>
+        <th>Розничная цена</th>
+        <th>Общая розничная цена</th>
+        <th></th>
+      </tr>
+      <tr>
+        <td><span><input value="Ноутбук ASUS X556UB-XO035T" type="text"></span></td>
+        <td>
+         <div class="input-group">
+  <input type="button" value="-" class="button-minus" data-field="quantity">
+  <input type="number" step="1" max="" value="1" name="quantity" class="quantity-field">
+  <input type="button" value="+" class="button-plus" data-field="quantity">
+</div>
+        </td>
+        <td><input value="34981" type="text"></td>
+        <td><input value="34981" type="text"></td>
+        <td><button><img src="img/trash.svg" alt=""></button></td>
+      </tr>
+      <tr>
+        <td><span><input value="Ноутбук ASUS X556UB-XO035T" type="text"></span></td>
+        <td>
+<div class="input-group">
+  <input type="button" value="-" class="button-minus" data-field="quantity">
+  <input type="number" step="1" max="" value="1" name="quantity" class="quantity-field">
+  <input type="button" value="+" class="button-plus" data-field="quantity">
+</div>
+        </td>
+        <td><input value="34981" type="text"></td>
+        <td><input value="34981" type="text"></td>
+        <td><button><img src="img/trash.svg" alt=""></button></td>
+      </tr>
+      <tr>
+        <td><span><input value="Ноутбук ASUS X556UB-XO035T" type="text"></span></td>
+        <td>
+          <div class="input-group">
+  <input type="button" value="-" class="button-minus" data-field="quantity">
+  <input type="number" step="1" max="" value="1" name="quantity" class="quantity-field">
+  <input type="button" value="+" class="button-plus" data-field="quantity">
+</div>
+        </td>
+        <td><input value="34981" type="text"></td>
+        <td><input value="34981" type="text"></td>
+        <td><button><img src="img/trash.svg" alt=""></button></td>
+      </tr>
+      <tr>
+        <th class="bottom-table"><div class="flex-end">Итого:</div></th>
+        <th class="bottom-table">5</th>
+        <th class="bottom-table"></th>
+        <th class="bottom-table">174 905 р.</th>
+        <th class="bottom-table"></th>
+      </tr>
+    </table>
+    </div>
+    <div class="flex-center">
+      <div>
+        <a class="flex-center" href="">Сохранить</a>
+      </div>
+    </div>
+  </div>
+          </div>
+        </div>
+    </div>
+</div>    
+</section>
+</main>
+
+
+
+
+
+
+<?php include("footer.php"); ?>
+
+
+
+
